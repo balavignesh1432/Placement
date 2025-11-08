1a3f43676751ddb98a591ddbb75e4e690ec6b94f
Bala Vignesh Ramasamy <balavignesh1432@gmail.com>
2025-11-08 12:57:45 +0530

    Detect Cycle in Linked list Complete

---
+++ Neetcode/Blind 75/detect_cycle_linked_list.py
@@ -0,0 +1,33 @@
+def hasCycle(self, head):
+    # visited = set()
+    # while head:
+    #     if head not in visited:
+    #         visited.add(head)
+    #     else:
+    #         return True
+    #     head = head.next
+    # return False
+
+    # Tortoise and Hare Cycle Detection
+    fast = slow = head
+    while slow and fast and fast.next: 
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:   
+            return True
+    return False
+
+
+    # Recursive Version
+    # TC - O(N), SC = O(N) [Call Stack]
+    def helper(slow, fast):
+        if not slow or not fast:
+            return False
+        if slow == fast:
+            return True
+        if fast.next:
+            return helper(slow.next, fast.next.next)
+        return False
+    if head and head.next:
+        return helper(head.next, head.next.next)
+    return False
