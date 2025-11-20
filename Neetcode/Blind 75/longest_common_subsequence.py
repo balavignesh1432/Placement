ac5125d4edc9961a428575cfc1814420f198733f
Bala Vignesh Ramasamy <balavignesh1432@gmail.com>
2025-11-20 19:19:23 +0530

    Longest Common Subsequence Space Optimisation

--- Neetcode/Blind 75/longest_common_subsequence.py
+++ Neetcode/Blind 75/longest_common_subsequence.py
@@ -20,7 +20,7 @@ def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         return dp[index1][index2]
     return helper(0, 0)
         
-    # Iteration DP:
+    # Iteration DP: Tabulation
     # TC: O(M * N), SC: O(M * N)
     dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
     for index1 in range(len(text1) - 1, -1, -1):
@@ -30,3 +30,22 @@ def longestCommonSubsequence(self, text1: str, text2: str) -> int:
             else:
                 dp[index1][index2] = max(dp[index1 + 1][index2], dp[index1][index2 + 1])
     return dp[0][0]
+
+    # Bottom Up DP: Space Optimisation
+    # TC: O(M*N), SC: O(min(M, N))
+    if len(text2) > len(text1):     # Text2 has to be smaller, since arrays intialized with its size.
+        text2, text1 = text1, text2 
+    
+    dp = [0] * (len(text2) + 1)
+    row = [0] * (len(text2) + 1)
+    for index1 in range(len(text1) - 1, -1, -1):
+        for index2 in range(len(text2) - 1, -1, -1):
+            if text1[index1] == text2[index2]:
+                dp[index2] = 1 + row[index2 + 1]
+            else:
+                dp[index2] = max(dp[index2 + 1], row[index2])
+        row, dp = dp, row       # Update row for next iteration, DP has to be valid row,
+        # Just swap with row instead of creating new one with 0s. 
+        # Saves time and only last value of dp has to be 0, rest will be calculated
+        # But just using row = dp, Will be overwritten since pass by reference, both will be same array
+    return row[0]
