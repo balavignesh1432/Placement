f8eba204f6e10bd77959f7c6fbb27771c438b570
Bala Vignesh Ramasamy <balavignesh1432@gmail.com>
2025-11-25 10:53:56 +0530

    Dijkstra Algo Complete

--- dijkstra_distance_array_update.py
+++ dijkstra_distance_array_update.py
@@ -9,22 +9,35 @@ class Solution:
     # As those distance will add to distance needed to reach this node
     # Otherwise, Go to neighbors, and check if edge weight + distance needed is less than dist[neigh],
     # This means shorter distance is found to reach neighbor, so update distance array, and add to heap
-    def dijkstra(self, V, edges, src):
-        adj = [[] for _ in range(V)]
-        dist = [float('inf') for _ in range(V)]
-        dist[src] = 0
+    # TC: O(E log V), SC: O(V + E),
+    # Since E = V2, for complete graph, log E = log V^2 = 2 log V
+    def dijkstra(vertices, edges, source):
+        dist = [0x7FFFFFFF for _ in range(vertices)]
+        dist[source] = 0
+        heap = []
+        heappush(heap, [0, source])
+        adj = [[] for _ in range(vertices)]
+        par = [node for node in range(vertices)]
         for edge in edges:
             adj[edge[0]].append([edge[1], edge[2]])
             adj[edge[1]].append([edge[0], edge[2]])
-        
-        heap = []
-        heappush(heap, [0, src])
         while len(heap):
-            weight, node = heappop(heap)
-            if weight > dist[node]:
+            du, u = heappop(heap)
+            if du < dist[u]:
                 continue
-            for neigh, d in adj[node]:
-                if weight + d < dist[neigh]:
-                    dist[neigh] = weight + d
-                    heappush(heap, [dist[neigh], neigh])
+            for v, dv in adj[u]:
+                if du + dv < dist[v]:
+                    dist[v] = du + dv
+                    par[v] = u          # Can be used to find the path from source to any target
+                    heappush(heap, [dist[v], v])
         return dist
+
+    def findPath(src, target, par): # Src is can not be different than one used for algo
+        cur = target
+        path = []
+        while cur != src:       # Until node reaching src
+            path.append(cur)    # Add node to path
+            cur = par[cur]      # Go to node's parent
+        path.append(src)        # Append src to path
+        path.reverse()          # Reverse it
+        return path
