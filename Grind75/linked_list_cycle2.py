a3595444a02844fb22079c4b97295352b19fa7e0
Bala Vignesh Ramasamy <balavignesh1432@gmail.com>
2025-12-29 18:40:30 +0530

    Linked List Cycle 2 Revision

--- Grind75/linked_list_cycle2.py
+++ Grind75/linked_list_cycle2.py
@@ -1,4 +1,16 @@
 def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    # Brute Force: Using Set to find if visited
+    # If visited already, then that is the start of the cycle
+    # If end reached, then no cycle
+    # TC: O(N), SC: O(N)
+    visited = set()    
+    while head:
+        if head in visited:
+            return head
+        visited.add(head)
+        head = head.next
+    return None
+
     # Two pointers:
     # Get the point where it meets
     # Let A be dist from head to start of chain, B be dist from start to meet
