9992a091dae580a88bd61fd73bd00cdef3553ee1
Bala Vignesh Ramasamy <balavignesh1432@gmail.com>
2025-12-30 19:32:42 +0530

    Valid Sudoku Revision

--- Grind75/valid_sudoku.py
+++ Grind75/valid_sudoku.py
@@ -11,15 +11,10 @@ def isValidSudoku(self, board: List[List[str]]) -> bool:
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j].isdigit():
-                if board[i][j] in row[i]:
+                if board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in box[(i // 3, j // 3)]:
                     return False
                 row[i].add(board[i][j])
-                if board[i][j] in col[j]:
-                    return False
                 col[j].add(board[i][j])
-                cell = (i // 3, j // 3)                
-                if board[i][j] in box[cell]:
-                    return False
-                box[cell].add(board[i][j])
+                box[(i // 3, j // 3)].add(board[i][j])
     return True
             
